261cc98baf8daff73089d209aee843b2b3ad3c6e
Hyon Woo Kang <hwkang@engistech.com>
2022-10-05 12:36:45 +0900

    화면 배치 및 변수 선언

--- MapView/ViewController.swift
+++ MapView/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet var myMap: MKMapView!
+    @IBOutlet var lbLocation1: UILabel!
+    @IBOutlet var lbLocation2: UILabel!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        lbLocation1.text = ""
+        lbLocation2.text = ""
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest   // 위치 정확도 최대
+        locationManager.requestWhenInUseAuthorization() // 위치 데이터 사용자 승인 요청
+        locationManager.startUpdatingHeading()  // 위치 정보 업데이트
+        myMap.showsUserLocation = true  // 현재 위치 보여주기
     }
 
-
+    @IBAction func segChnageLocation(_ sender: UISegmentedControl) {
+        
+        
+    }
+    
 }
 
